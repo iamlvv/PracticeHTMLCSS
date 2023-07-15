174bbd3cfd06bde24a1a6df05f49e434ac5bc396
iamlvv <levanvy28062002@gmail.com>
2023-07-15 17:28:47 +0700

    add sliders but not optimizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,6 @@ navbarItemPage.forEach((item) => {
   item.addEventListener("mouseover", () => {
     item.style.color = "#d64d00";
     item.style.cursor = "pointer";
-    item.style.transitionDuration = "0.25s";
-    item.style.transitionTimingFunction = "ease-in-out";
   });
   item.addEventListener("mouseout", () => {
     item.style.color = "#000";
@@ -18,7 +16,7 @@ searchButton.forEach((button) => {
   button.addEventListener("mouseover", () => {
     button.style.cursor = "pointer";
   });
-  button.addEventListener("mouseout", () => {});
+  button.addEventListener("mouseout", () => { });
 });
 
 //Add effects when hover on the shop-now button using vanilla javascript
@@ -70,3 +68,45 @@ collectionNavbarItem.forEach((item) => {
     item.classList.add("collection-navbar-item-current");
   });
 });
+
+// Build draggable slider using vanilla javascript
+const userReviewsContainer = document.querySelector(".user-reviews-container");
+const userReviews = document.querySelector(".user-reviews");
+
+let isPressedDown = false;
+let cursorXSpace;
+
+userReviewsContainer.addEventListener("mousedown", (e) => {
+  isPressedDown = true;
+  cursorXSpace = e.offsetX - userReviews.offsetLeft;
+  userReviewsContainer.style.cursor = "grabbing";
+});
+
+userReviewsContainer.addEventListener("mouseup", () => {
+  // isPressedDown = false;
+  userReviewsContainer.style.cursor = "grab";
+});
+window.addEventListener("mouseup", () => {
+  isPressedDown = false;
+});
+
+userReviewsContainer.addEventListener("mousemove", (e) => {
+  if (!isPressedDown) return;
+  e.preventDefault();
+  userReviews.style.left = `${e.offsetX - cursorXSpace}px`;
+  boundary();
+});
+
+
+function boundary() {
+  const container = userReviewsContainer.getBoundingClientRect();
+  const general = userReviews.getBoundingClientRect();
+
+  if (parseInt(userReviews.style.left) > 0) {
+    userReviews.style.left = "0px";
+  } else if (general.right < container.right) {
+    userReviews.style.left = `-${general.width - container.width}px`;
+  }
+}
+
+
